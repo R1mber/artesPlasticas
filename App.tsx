4960ba30d93df9ebaa6c6171c33d1af72819cfe8
Leonel <leonel.sis12@gmail.com>
2023-05-18 22:19:09 -0400

    Leo

--- App.tsx
+++ App.tsx
@@ -3,13 +3,28 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/screens/home';
 import Draw from './src/screens/draw';
+import Tema from './src/screens/tema';
 
 const Stack = createNativeStackNavigator();
 function App(): JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Tema"
+          component={Tema}
+          options={{
+            headerShown: false,
+          }}
+        />
+
         <Stack.Screen name="Draw" component={Draw} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/utils/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  gray: '#757780',
+  mostaza: '#d2cca1',
+  verde: '#387780',
+  light: '#dbd4d3',
+  red: '#e83151',
+};
